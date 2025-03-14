1403982ab8b437234b143c32f6b7c984ed4e5da9
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-03-14 15:37:14 +0530

    reorg

--- matplotlib_test.py
+++ matplotlib_test.py
@@ -19,7 +19,7 @@ plt.ylabel("Calorie Burnage",fontdict=font2)
 plt.plot(x,y)
 plt.grid(color='green',ls='--',linewidth=0.5)
 plt.show()
-'''
+
 #subplots:
 #----------------
 #plot 1
@@ -34,4 +34,32 @@ y2_sub=np.array([10,20,30,40])
 plt.subplot(1,2,2)
 plt.plot(x2_sub,y2_sub)
 
+x_sub2=np.array([0,1,2,3])
+y_sub2=np.array([3,4,6,7])
+
+plt.subplot(2,1,1)
+plt.plot(x_sub2,y_sub2)
+plt.title("Sales")
+
+x2_sub2=np.array([0,1,2,3])
+y2_sub2=np.array([10,20,30,40])
+
+plt.subplot(2,1,2)
+plt.plot(x2_sub2,y2_sub2)
+plt.title("Income")
+
+plt.suptitle("Goods and Services")
 plt.show()
+'''
+#===========================================
+#MATPLOTLIB SCATTER:
+#==========================================
+#Creating Scatter plots:
+#-----------------------
+x_scatter=np.array([1,2,3,4,5,6,7,8,9,10])
+y_scatter=np.array([5,3,6,9,24,2,3,7,13,24])
+
+plt.scatter(x_scatter,y_scatter)
+plt.show()
+
+
