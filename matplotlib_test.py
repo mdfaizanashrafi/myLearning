e26c219ec0174a95c3474ae56e306295e88539f0
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-03-14 15:57:41 +0530

    Finished Matplotlib

--- matplotlib_test.py
+++ matplotlib_test.py
@@ -50,7 +50,7 @@ plt.title("Income")
 
 plt.suptitle("Goods and Services")
 plt.show()
-'''
+
 #===========================================
 #MATPLOTLIB SCATTER:
 #==========================================
@@ -60,6 +60,44 @@ x_scatter=np.array([1,2,3,4,5,6,7,8,9,10])
 y_scatter=np.array([5,3,6,9,24,2,3,7,13,24])
 
 plt.scatter(x_scatter,y_scatter)
+
+#Compare plots:
+#day 1
+x_comp=np.array([1,2,3,4,5,6,7,8,9,10])
+y_comp=np.array([5,3,6,9,24,2,3,7,13,24])
+
+plt.scatter(x_comp,y_comp,c='hotpink')
+
+#day 2
+x_comp2=np.array([9,3,2,14,15,6,28,9,10,23])
+y_comp2=np.array([5,7,4,2,6,17,4,34,3,9])
+
+plt.scatter(x_comp2,y_comp2,color='green1')
+
 plt.show()
 
+#=============================================
+#Matplotlib Bars:
+#----------------------
+x_bar= np.array(["A","B","C","D","E","F","G","H","I","J"])
+y_bar=np.array([3,4,5,6,7,8,9,10,11,12])
 
+plt.barh(x_bar,y_bar,color='green',height=0.5)
+plt.show() 
+
+#=============================================
+# #Matplotlib Histogram:
+# ----------------------
+x_hist= np.random.normal(170,10,250)
+plt.hist(x_hist)
+plt.show()
+'''
+#Matplotlib Pie Chart:
+#---------------------
+y_pie=np.array([30,25,20,40])
+my_labels=["A","B","C","D"]
+myexoplode=[0,0.1,0,0]
+
+plt.pie(y_pie,labels=my_labels,startangle=90,explode=myexoplode,shadow=True)
+plt.legend(title="Legend Title")
+plt.show()
